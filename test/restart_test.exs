298a26412e2e1aafe8201a6ea5015f5f5e3c4c29
Bengt Kleberg <bengt.kleberg@travelping.com>
2024-08-01 12:11:50 +0200

    Try to remove timing problems

--- test/restart_test.exs
+++ test/restart_test.exs
@@ -3,13 +3,9 @@ defmodule NatsTestIex.RestartTest do
 
   @moduletag timeout: 10_000
 
-  test "CDR started" do
-    assert Process.alive?(Process.whereis(NatsTestIex.CDR))
-  end
-
   test "publish one ack" do
     pid = NatsTestIex.TestHelper.cdr_start(%{reply: :ack, testcase: :one_ack})
-    Process.sleep(999)
+    NatsTestIex.TestHelper.cdr_empty()
     m = %{n: 11, apn: "one_ack"}
     Gnat.pub(:gnat, "cdr", :erlang.term_to_binary(m))
     cdr = NatsTestIex.TestHelper.cdr_get_one()
@@ -21,7 +17,7 @@ defmodule NatsTestIex.RestartTest do
 
   test "publish one noreply" do
     pid = NatsTestIex.TestHelper.cdr_start(%{reply: :noreply, testcase: :one_noreply})
-    Process.sleep(999)
+    NatsTestIex.TestHelper.cdr_empty()
     m = %{n: 12, apn: "one_noreply"}
     Gnat.pub(:gnat, "cdr", :erlang.term_to_binary(m))
     cdr = NatsTestIex.TestHelper.cdr_get_one()
@@ -33,6 +29,7 @@ defmodule NatsTestIex.RestartTest do
 
   test "publish two" do
     pid = NatsTestIex.TestHelper.cdr_start(%{testcase: :two})
+    NatsTestIex.TestHelper.cdr_empty()
     m = %{n: 2, apn: "two"}
     Gnat.pub(:gnat, "cdr", :erlang.term_to_binary(m))
     cdr = NatsTestIex.TestHelper.cdr_get_one()
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,14 +1,42 @@
 ExUnit.start()
 
 defmodule NatsTestIex.TestHelper do
+  def cdr_empty() do
+    cdr_await_process(nil)
+    # Allow old items in stream to propagate
+    Process.sleep(999)
+    NatsTestIex.CDR.get() |> cdr_await_empty()
+  end
+
   def cdr_get_one(), do: NatsTestIex.CDR.get() |> cdr_get_one()
-  def cdr_start(), do: Process.spawn(fn -> cdr_start_wait(%{}) end, [])
-  def cdr_start(config), do: Process.spawn(fn -> cdr_start_wait(config) end, [])
+  def cdr_start(), do: cdr_start(%{})
+
+  def cdr_start(config) do
+    s = Kernel.self()
+    pid = Process.spawn(fn -> cdr_start_wait(config, s) end, [])
+
+    receive do
+      {:started, ^pid} -> :ok
+    end
+
+    pid
+  end
+
   def cdr_stop(pid, kind), do: Process.exit(pid, kind)
 
   #
   # Internal functions
   #
+
+  defp cdr_await_empty([]), do: :ok
+
+  defp cdr_await_empty([old]) do
+    IO.puts("Unempty CDR: #{Kernel.inspect(old)}")
+    NatsTestIex.CDR.get() |> cdr_await_empty()
+  end
+
+  defp cdr_await_process(nil), do: NatsTestIex.CDR |> Process.whereis() |> cdr_await_process()
+  defp cdr_await_process(_), do: :ok
 
   defp cdr_get_one([]) do
     Process.sleep(900)
@@ -17,8 +45,9 @@ defmodule NatsTestIex.TestHelper do
 
   defp cdr_get_one([one]), do: one
 
-  defp cdr_start_wait(config) do
+  defp cdr_start_wait(config, reply_to) do
     _ = NatsTestIex.CDRPullConsumer.start_link(config)
+    Process.send(reply_to, {:started, Kernel.self()}, [])
     Process.sleep(100_000)
   end
 end
